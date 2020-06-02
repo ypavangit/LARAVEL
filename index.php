2a25f4e67537043028826f6bf57254f9da81a085
ypavangit <ypavandeveloper@gmail.com>
2020-06-02 10:04:59 +0530

    add new echo

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
 echo 'welcome to laravel learning';
+echo 'welcome to laravel learning';
+echo 'welcome to laravel learning';
+echo 'welcome to laravel learning';
+echo 'welcome to laravel learning';
+echo 'welcome to laravel learning';
+echo 'welcome to laravel learning';
 ?>
